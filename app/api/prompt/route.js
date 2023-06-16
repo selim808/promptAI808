cdffc303ad67fe684245c9a4e7ca24e5a9acac64
selim808 <selim_808@hotmail.com>
2023-06-16 15:41:23 +0300

    second commit

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,14 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
-export const GET = async (req) => {
+import { connectToDB } from "@utils/database";
+
+export const GET = async (request) => {
   try {
     await connectToDB();
+
     const prompts = await Prompt.find({}).populate("creator");
-    console.log(prompts);
-    return new Response(JSON.stringify(prompts), { status: 201 });
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    console.log(error);
-    return new Response(" Failed to create a new prompt ", { status: 500 });
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
